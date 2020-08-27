ff5a37e8f0dba04e39ad86e5eec4758b27b25a15
Your Name <you@example.com>
2020-08-27 14:27:47 -0300

    3

--- Ejercicio03/Program.cs
+++ Ejercicio03/Program.cs
@@ -14,6 +14,40 @@ namespace Ejercicio03
     {
         static void Main(string[] args)
         {
+            Console.Title = "Ejercicio Nro 03";
+            
+            Console.WriteLine("Ingrese un numero:");
+            Int32 numero = int.Parse(Console.ReadLine());
+            
+
+            if(numero<1)
+            {
+                Console.WriteLine("Error. debe ser mayor a 0");
+            }
+            else if(numero==1)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine("Los numeros primos hasta el {0} son:", numero);
+                for(int i=numero-1;i>=0;i--)
+                {
+                    byte flag = 0;
+                    for(int y=2;y<i;y++)
+                    {
+                        if(i%y==0)
+                        {                      
+                            flag = 1;
+                            break;
+                        }
+                    }
+                    if (flag == 0)
+                    {
+                        Console.Write(" {0}", i);
+                    }
+                }
+            }
             Console.ReadKey(true);
         }
     }
