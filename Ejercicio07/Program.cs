16a9a05569f00ecbd4bd83214a95bb2a4436c302
Your Name <you@example.com>
2020-08-27 21:56:31 -0300

    8

--- Ejercicio07/Program.cs
+++ Ejercicio07/Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+
+/*Hacer un programa que pida por pantalla la fecha de nacimiento de una persona (día, mes y año) y
+calcule el número de días vividos por esa persona hasta la fecha actual (tomar la fecha del sistema
+con DateTime.Now).
+Nota: Utilizar estructuras selectivas. Tener en cuenta los años bisiestos*/
 
 namespace Ejercicio07
 {
@@ -11,7 +17,27 @@ namespace Ejercicio07
         static void Main(string[] args)
         {
             Console.Title = "Ejercicio Nro 07";
+            Int32 anio;
+            Int32 mes;
+            Int32 dia;
+            Int64 totalDias;
 
+            Console.WriteLine("Dia de nacimiento[01/]: ");
+            dia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Mes de nacimiento[/02/]: ");
+            mes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Año de nacimiento[/1998]: ");
+            anio = int.Parse(Console.ReadLine());
+            
+            DateTime birth = new DateTime(anio, mes, dia,0,0,0);
+            DateTime now = DateTime.Now;
+
+            TimeSpan diferencia = now-birth;
+            totalDias = (int)diferencia.TotalDays;/*paso de double a int asi no redondea*/
+            
+            Console.WriteLine("{0}", birth);
+            Console.WriteLine("{0}", now);
+            Console.WriteLine("diferencia de dias: {0:##,###.00}", totalDias);
 
             Console.ReadKey(true);
         }
---
+++ Ejercicio08/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Por teclado se ingresa el valor hora, el nombre, la antigüedad (en años) y la cantidad de horas
+trabajadas en el mes de N empleados de una fábrica.
+Se pide calcular el importe a cobrar teniendo en cuenta que el total (que resulta de multiplicar el
+valor hora por la cantidad de horas trabajadas), hay que sumarle la cantidad de años trabajados
+multiplicados por $ 150, y al total de todas esas operaciones restarle el 13% en concepto de
+descuentos.
+Mostrar el recibo correspondiente con el nombre, la antigüedad, el valor hora, el total a cobrar en
+bruto, el total de descuentos y el valor neto a cobrar de todos los empleados ingresados.
+Nota: Utilizar estructuras repetitivas y selectivas.*/
+namespace Ejercicio08
+{
+    class Ejercicio_07
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 08";
+
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
