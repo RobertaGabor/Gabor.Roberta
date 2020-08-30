e6552b49c24d4c614b6aede869ebe602beb18d7e
Your Name <you@example.com>
2020-08-30 14:33:29 -0300

    11

---
+++ Ejercicio11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*11. Realizar una clase llamada Validacion que posea un método estático llamado Validar, que posea la
+siguiente firma: bool Validar(int valor, int min, int max):
+a. valor: dato a validar
+b. min y max: rango en el cual deberá estar la variable valor.
+Pedir al usuario que ingrese 10 números enteros. Validar con el método desarrollado
+anteriormente que esten dentro del rango -100 y 100.
+Terminado el ingreso mostrar el valor mínimo, el valor máximo y el promedio.
+Nota: Utilizar variables escalares, NO utilizar vectores.*/
+
+namespace Ejercicio11
+{
+    class Ejercicio_11
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 11";
+
+
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
