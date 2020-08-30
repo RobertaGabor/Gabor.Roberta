62c72be32dfb9aa3519107248b5ed5518a3a698f
Your Name <you@example.com>
2020-08-30 15:22:06 -0300

    12

---
+++ Ejercicio12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*12. Realizar un programa que sume números enteros hasta que el usuario lo determine, por medio de
+un mensaje "¿Continuar? (S/N)".
+En el método estático ValidaS_N(char c) de la clase ValidarRespuesta, se validará el ingreso de
+opciones.
+El método devolverá un valor de tipo booleano, TRUE si se ingresó una 'S' y FALSE si se ingresó
+cualquier otro valor*/
+namespace Ejercicio12
+{
+    class Ejercicio_12
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 12";
+
+
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
