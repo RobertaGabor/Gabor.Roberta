f73cd6130d5adf5921d8efd70bdaa3835e3721b0
Your Name <you@example.com>
2020-08-30 16:22:02 -0300

    13

---
+++ Ejercicio13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio13
+{
+    class Ejercicio_13
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 13";
+
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
