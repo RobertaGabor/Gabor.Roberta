9cae5141e0c46af325a9f0bfd4a308007eeedc03
Your Name <you@example.com>
2020-09-07 15:53:40 -0300

    14

---
+++ Ejercicio14/CalculoDeArea.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio14
+{
+    class CalculoDeArea
+    {
+
+        public static double CalcularCuadrado(double lado)
+        {
+            return lado * lado;
+        }
+        public static double CalcularTriangulo(double base1, double altura)
+        {
+            return base1 * altura / 2;
+        }
+        public static double CalcularCirculo(double radio)
+        {
+            return radio * radio * Math.PI;
+        }
+    }
+}
---
+++ Ejercicio14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*14. Realizar una clase llamada CalculoDeArea que posea 3 métodos de clase (estáticos) que realicen el
+cálculo del área que corresponda:
+a. double CalcularCuadrado(double)
+b. double CalcularTriangulo(double, double)
+c. double CalcularCirculo(double)
+El ingreso de los datos como la visualización se deberán realizar desde el método Main().*/
+namespace Ejercicio14
+{
+    class Ejercicio_14
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 14";
+            Double areaCirculo;
+            Double areaCuadrado;
+            Double areaTriangulo;
+
+            areaCirculo = CalculoDeArea.CalcularCirculo(2);
+            areaCuadrado = CalculoDeArea.CalcularCuadrado(6);
+            areaTriangulo = CalculoDeArea.CalcularTriangulo(4, 3);
+
+            Console.WriteLine("area de circulo: {0:##,###.00}, area de triangulo: {1:##,###.00}, area de cuadrado: {2:##,###.00}", areaCirculo, areaTriangulo, areaCuadrado);
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
