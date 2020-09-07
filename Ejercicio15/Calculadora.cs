7337e755cd929b14498c1f7d32e76af7d3e3d036
Your Name <you@example.com>
2020-09-07 16:54:03 -0300

    15

---
+++ Ejercicio15/Calculadora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio15
+{
+    class Calculadora
+    {
+        /// <summary>
+        /// realiza una operacion entre las mostradas y si se puede deuelve el numero, sino devuelve -1111111111
+        /// </summary>
+        /// <param name="x">numero 1 ingresado</param>
+        /// <param name="y">numero 2 ingresado</param>
+        /// <param name="operacion">metodo de operacion</param>
+        /// <returns></returns>
+        public static Double Calcular(Int32 x, Int32 y, char operacion)
+        {
+            Double resultado=-111111111;
+
+            if(operacion=='+')
+            {
+                resultado = x + y;
+            }
+            else if(operacion=='-')
+            {
+                resultado = x - y;
+            }
+            else if(operacion=='*')
+            {
+                resultado = x * y;
+            }
+            else if(operacion=='/')
+            {
+                if(Validar(y)==true)
+                {
+                    resultado = (float)x / y;
+                }
+            }
+
+            return resultado;
+        }
+
+        private static bool Validar(Int32 y)
+        {
+            bool ok = false;
+            if (y != 0)
+            {
+                ok = true;
+            }
+
+            return ok;
+        }
+    }
+}
---
+++ Ejercicio15/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*15. Realizar un programa que permita realizar operaciones matemáticas simples (suma, resta,
+multiplicación y división). Para ello se le debe pedir al usuario que ingrese dos números y la
+operación que desea realizar (pulsando el caracter +, -, * ó /).
+El usuario decidirá cuándo finalizar el programa.
+Crear una clase llamada Calculadora que posea dos métodos estáticos (de clase):
+a. Calcular (público): Recibirá tres parámetros, el primer número, el segundo número y la
+operación matemática. El método devolverá el resultado de la operación.
+b. Validar (privado): Recibirá como parámetro el segundo número. Este método se debe
+utilizar sólo cuando la operación elegida sea la DIVISIÓN. Este método devolverá
+TRUE si el número es distinto de CERO.*/
+
+namespace Ejercicio15
+{
+    class Ejercicio_15
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 15";
+
+            Int32 numero1;
+            Int32 numero2;
+            char operacion;
+            char seguir = 's';
+            Double resultado;
+
+            while(seguir=='s')
+            {
+                Console.WriteLine("Escriba la operacion(+,-,/,*) que desee: ");
+                operacion = char.Parse(Console.ReadLine());
+                Console.WriteLine("A {0} B = ", operacion);
+
+                Console.WriteLine("Escriba el valor de A: ");
+                numero1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Escriba el valor de B: ");
+                numero2 = int.Parse(Console.ReadLine());
+
+                resultado = Calculadora.Calcular(numero1, numero2, operacion);
+                Console.WriteLine("{0} {1} {2} = {3:##,###.00}", numero1, operacion, numero2,resultado);
+
+                Console.WriteLine("Desea calcular otra cosa? s/n: ");
+                seguir = char.Parse(Console.ReadLine());
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
