6f3b3e91546ab22ff31d74e4667b49de3778c6e5
Roberta <robyrubirex@gmail.com>
2020-09-26 03:35:21 -0300

    28

---
+++ Ejercicio28/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Ejercicio28
+{
+    partial class ContadorForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxCalcular = new System.Windows.Forms.RichTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxCalcular
+            // 
+            this.richTextBoxCalcular.Location = new System.Drawing.Point(12, 12);
+            this.richTextBoxCalcular.Name = "richTextBoxCalcular";
+            this.richTextBoxCalcular.Size = new System.Drawing.Size(464, 386);
+            this.richTextBoxCalcular.TabIndex = 0;
+            this.richTextBoxCalcular.Text = "";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(357, 404);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(109, 34);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // ContadorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.richTextBoxCalcular);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(504, 489);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(504, 489);
+            this.Name = "ContadorForm";
+            this.Text = "Contador de palabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxCalcular;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Ejercicio28/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio28
+{
+    public partial class ContadorForm : Form
+    {
+        public ContadorForm()
+        {
+            InitializeComponent();           
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, int> contador = new Dictionary<string, int>();
+            string texto = richTextBoxCalcular.Text;
+            string palabra="";
+            StringBuilder mensaje = new StringBuilder();
+            int fore = 0;
+
+            for(int i=0;i<texto.Length;i++)
+            {
+                if (char.IsWhiteSpace(texto[i])==false&&i!=texto.Length-1)
+                {
+                    palabra += texto[i];
+                }
+                else
+                {
+                    if(i == texto.Length - 1)
+                    {
+                        palabra += texto[i];
+                    }
+                    if (contador.ContainsKey(palabra)==true)
+                    {
+                        int y = contador[palabra] + 1;
+                        contador[palabra] = y;
+                    }
+                    else
+                    {
+                        contador.Add(palabra, 1);
+                    }
+                    palabra = "";
+
+                }
+
+            }
+
+
+            mensaje.AppendLine("TOP 3");
+            foreach (KeyValuePair<string, int> pare in contador.OrderBy(key=>key.Value).Reverse())          
+            {
+                mensaje.AppendLine($"{pare.Value} - {pare.Key}");
+                fore++;
+                if (fore == 3)
+                {
+                    break;
+                }
+            }
+
+            MessageBox.Show(mensaje.ToString());
+            
+
+
+        }
+
+    }
+}
